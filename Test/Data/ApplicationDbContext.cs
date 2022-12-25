54e113edb9408f8c7dd6b0f36bb5ae9ac1102e26
VladFit <108481948+VladFit@users.noreply.github.com>
2022-12-25 15:07:39 +0100

    Добавьте файлы проекта.

---
+++ Test/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test.Models;
+
+namespace Test.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+
+    }
+}
