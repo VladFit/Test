e67a1adfb055a8f99b180b7fb7a813a46060b5c1
VladFit <108481948+VladFit@users.noreply.github.com>
2023-01-27 15:34:08 +0100

    1. Добавляем представление для метода Index контроллера Запросов. 2. Создаем файл на JS для вывода списка Запросов в таблицу. 3. Настраиваем вывод списка Запросов.

--- Test/Controllers/InquiryController.cs
+++ Test/Controllers/InquiryController.cs
@@ -25,6 +25,15 @@ namespace Test.Controllers
         {
             return View();
         }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList()
+        {
+            return Json(new { data = _inqHRepo.GetAll() });
+        }
+
+        #endregion
     }
 }
 
