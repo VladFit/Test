06fa74cde4e1902a545f72220f610b56fc14ecf1
VladFit <108481948+VladFit@users.noreply.github.com>
2023-02-05 13:06:18 +0100

    1. Создаем ViewModel для управления списком Заказов. 2. Настраиваем метод Index для списка Заказов. 3. Установка Syncfusion. 4. Добавляем форматирование Syncfusion. 5. Настраиваем форматирование Syncfusion. 6. Настраиваем интерфейс фильтрации. 7. Настраиваем логику фильтрации списка Заказов. 8. Валидация данных для подтверждения Заказа.

---
+++ Test/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Test_DataAccess.Repository.IRepository;
+using Test_Models.ViewModels;
+using Test_Utility;
+using Test_Utility.BrainTree;
+
+namespace Test.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+        public OrderController(IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo,
+            IBrainTreeGate brain)
+        {
+            _brain = brain;
+            _orderDRepo = orderDRepo;
+            _orderHRepo = orderHRepo;
+        }
+
+        public IActionResult Index(string searchName = null, string searchEmail = null, string searchPhone = null, string Status = null)
+        {
+            OrderListVM orderListVM = new OrderListVM()
+            {
+                OrderHList = _orderHRepo.GetAll(),
+                StatusList = WC.listStatus.ToList().Select(i => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
+
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.FullName.ToLower().Contains(searchName.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchEmail))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.Email.ToLower().Contains(searchEmail.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(searchPhone))
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.PhoneNumber.ToLower().Contains(searchPhone.ToLower()));
+            }
+            if (!string.IsNullOrEmpty(Status) && Status != "--Order Status--")
+            {
+                orderListVM.OrderHList = orderListVM.OrderHList.Where(u => u.OrderStatus.ToLower().Contains(Status.ToLower()));
+            }
+
+            return View(orderListVM);
+        }
+    }
+}
--- Test_Utility/WC.cs
+++ Test_Utility/WC.cs
@@ -1,4 +1,7 @@
-﻿namespace Test_Utility
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Test_Utility
 {
     public static class WC
     {
@@ -22,5 +25,11 @@
         public const string StatusShipped = "Shipped";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static readonly IEnumerable<string> listStatus = new ReadOnlyCollection<string>(
+            new List<string>
+            {
+                StatusApproved, StatusCancelled, StatusInProcess, StatusPending, StatusRefunded, StatusShipped
+            });
     }
 }
